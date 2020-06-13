b53a6938a4d6717a18ab57e9ba2e3d3166a271af
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-06-13 12:13:53 -0300

    question 2 done

--- concept/challenge.js
+++ concept/challenge.js
@@ -25,4 +25,37 @@ console.log(admin1);
 const user1 = new User('rafaelbatistacg@gmail.com', '123');
 console.log(user1.getAdmin());
 
+
 // QUESTION 2
+var listUsers = [
+    { name: 'Diego', year: 23, company: 'Rocketseat' },
+    { name: 'Gabriel', year: 15, company: 'Rocketseat' },
+    { name: 'Lucas', year: 30, company: 'Facebook' },
+];
+
+console.log(listUsers.map( user => user.year ));
+
+console.log(listUsers.filter( (user) => {
+    if (user.year > 18 && user.company == 'Rocketseat') {
+        return user;
+    } 
+}))
+
+console.log(listUsers.find( user => user.company == 'Google' ));
+
+// console.log(listUsers.map( user => {
+//     if (user.year * 2 <= 50){
+//         return user;
+//     } 
+// }));
+
+var yearDuplicate = listUsers
+    .map( user => {
+        user.year *= 2;
+        return user; 
+    }) 
+    .filter( 
+        user => user.year <= 50
+    );
+
+console.log(yearDuplicate);
