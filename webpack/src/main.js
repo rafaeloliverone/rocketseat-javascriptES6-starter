eba8bbb5132b9604620a53840bdbf2a63f5d8a70
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-06-15 12:38:26 -0300

    structures, style and adding repositories

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const myPromise = () => new Promise((resolve, reject) => {
     setTimeout(() => { resolve('OK') }, 2000);
 });
@@ -8,3 +10,51 @@ async function executPromise() {
 }
 
 executPromise();
+
+// AXIOS
+
+class Api {
+    static async getUserInfo(username) {
+        try {
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response);
+        } catch (error) {
+            console.warn(error);
+        }
+    }
+}
+
+Api.getUserInfo('rafaeloliverone');
+
+
+// APLICATION ES6+
+
+class App {
+    constructor() {
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+
+        this.registerHandlers();
+
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event) {
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'rocketseat',
+            description: 'Tire a  sua ideia do papel e dê vida.',
+            avatar_url: 'https://avatars2.githubusercontent.com/u/48067346?v=4',
+            html_url: 'https://github.com/rafaeloliverone'
+        });
+
+        console.log(this.repositories);
+    }
+}
+
+new App();
